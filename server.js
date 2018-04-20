e9409fe031ec84b3a86e47043689fe5212b1e9a0
Jorrit Meeuwissen <jorritjeah@gmail.com>
2018-04-20 10:29:11 +0200

    server using express

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 
 const Person = require('./classes/Person.js');
 let random_name = require('node-random-name');
+let express = require('express');
+let app = express();
 
-let http = require('http');
+
 let port = process.env.PORT || 3000;
 
-
-
-http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/json'});
-    let json = [];
-    json.push(new Person("Jorrit", "Meeuwissen", 20));
+app.get('/', function (req, res) {
+    let data = [];
+    data.push(new Person("Jorrit", "Meeuwissen", 20));
     for (let i = 0; i < 10; i++) {
-        json.push(new Person(random_name({random: Math.random, first: true}), random_name({random: Math.random, last: true}), Math.floor(Math.random() * 50) + 15));
+        data.push(new Person(random_name({random: Math.random, first: true}), random_name({random: Math.random, last: true}), Math.floor(Math.random() * 50) + 15));
     }
-    response.end(JSON.stringify(json));
-}).listen(port);
+    res.send(JSON.stringify(data));
+});
+
+app.listen(port);
 
 console.log('Server created on port ' + port);
