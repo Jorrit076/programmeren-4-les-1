673902223a3522f6714f54577104f5c8ae6aff59
Jorrit Meeuwissen <jorritjeah@gmail.com>
2018-04-20 10:00:01 +0200

    Simplified code

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 
 const Person = require('./classes/Person.js');
 
-let onRequest = function(request, response) {
-    console.log('onRequest called');
-    response.writeHead(200, {'Content-Type': 'text/json'});
-    response.end(getData());
-};
-
-let getData = function() {
-    console.log('getData called');
-
-    let json = [];
-    json.push(new Person("Jorrit", "Meeuwissen", 20));
-    json.push(new Person("Maud", "Jacobs", 18));
-
-    return JSON.stringify(json);
-};
-
 let http = require('http');
 let port = process.env.PORT || 3000;
-http.createServer(onRequest).listen(port);
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/json'});
+    let json = [];
+    json.push(new Person("Jorrit", "Meeuwissen", 20));
+    json.push(new Person("Femke", "Lambers", 18));
+    response.end(JSON.stringify(json));
+}).listen(port);
 
 console.log('Server created on port ' + port);
