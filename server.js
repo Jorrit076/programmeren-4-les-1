b4dd3dee7a9cf210cf3122002172600ea57b1ad3
Jorrit Meeuwissen <jorritjeah@gmail.com>
2018-04-20 10:21:33 +0200

    random name generator

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 
 const Person = require('./classes/Person.js');
+let random_name = require('node-random-name');
 
 let http = require('http');
 let port = process.env.PORT || 3000;
+
+
+
 http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/json'});
     let json = [];
     json.push(new Person("Jorrit", "Meeuwissen", 20));
-    json.push(new Person("Femke", "Lambers", 18));
+    for (let i = 0; i < 10; i++) {
+        json.push(new Person(random_name({random: Math.random, first: true}), random_name({random: Math.random, last: true}), Math.floor(Math.random() * 50) + 15));
+    }
     response.end(JSON.stringify(json));
 }).listen(port);
 
